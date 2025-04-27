6c0f09ad127090343834240556c543f028ee8fac
unknown <fullstackgabe@gmail.com>
2025-04-27 20:01:30 -0300

    command

--- exemplos/comportamentais/Command/index.php
+++ exemplos/comportamentais/Command/index.php
@@ -1,110 +1,88 @@
 <?php
-// Comando.php - Interface que define o contrato dos comandos
+
+// Interface Comando
 interface Comando {
     public function executar();
 }
 
-// PedidoComando.php - Comando que representa um pedido feito pelo cliente
+// Comando para registrar o pedido
 class PedidoComando implements Comando {
     private $pedido;
-    private $garcom;
 
-    // Injetando o garçom para que o comando de pedido o registre
-    public function __construct($pedido, Garcom $garcom) {
+    public function __construct($pedido) {
         $this->pedido = $pedido;
-        $this->garcom = $garcom;
     }
 
     public function executar() {
         echo "Pedido recebido: " . $this->pedido . PHP_EOL;
-        $this->garcom->adicionarPedido($this);  // Adicionando pedido à fila do garçom
-    }
-
-    public function getPedido() {
-        return $this->pedido;
     }
 }
 
-// CozinharComando.php - Comando que representa o ato de cozinhar o pedido
+// Comando para cozinhar o pedido
 class CozinharComando implements Comando {
     private $pedido;
-    private $chef;
 
-    // Injetando o chef para que o comando de cozinhar o execute
-    public function __construct($pedido, Chef $chef) {
+    public function __construct($pedido) {
         $this->pedido = $pedido;
-        $this->chef = $chef;
     }
 
     public function executar() {
-        $this->chef->cozinhar($this->pedido);  // Chef cozinha o pedido
+        echo "Cozinhando a refeição: " . $this->pedido . PHP_EOL;
     }
 }
 
-// ServirComando.php - Comando que representa o ato de servir a refeição
+// Comando para servir o pedido
 class ServirComando implements Comando {
     private $pedido;
-    private $garcom;
 
-    // Injetando o garçom para servir a refeição
-    public function __construct($pedido, Garcom $garcom) {
+    public function __construct($pedido) {
         $this->pedido = $pedido;
-        $this->garcom = $garcom;
     }
 
     public function executar() {
-        $this->garcom->servir($this->pedido);  // Garçom serve a refeição
+        echo "Servindo a refeição: " . $this->pedido . PHP_EOL;
     }
 }
 
-// Chef.php - Receptor que cozinha a refeição
-class Chef {
-    public function cozinhar($pedido) {
-        echo "Cozinhando a refeição: " . $pedido . PHP_EOL;
-    }
-}
-
-// Garcom.php - Receptor que serve a refeição
+// O garçom (Invocador) que recebe e executa os comandos
 class Garcom {
-    private $filaDePedidos = [];
+    private $comandos = [];
 
-    public function adicionarPedido(PedidoComando $pedido) {
-        $this->filaDePedidos[] = $pedido;
+    public function adicionarComando(Comando $comando) {
+        $this->comandos[] = $comando;
     }
 
-    public function servir($pedido) {
-        echo "O garçom está servindo a refeição: " . $pedido . PHP_EOL;
-    }
-
-    public function executarPedidos() {
-        foreach ($this->filaDePedidos as $pedido) {
-            echo "Processando o pedido: " . $pedido->getPedido() . PHP_EOL;
+    public function executarComandos() {
+        foreach ($this->comandos as $comando) {
+            $comando->executar();
         }
     }
 }
 
-// Código principal (index.php)
-$garcom = new Garcom();  // Criando o garçom (invocador)
-$chef = new Chef();  // Criando o chef (receptor)
+// Código principal
+$pedido1 = "Hambúrguer com batatas fritas";
+$pedido2 = "Salada Caesar";
 
-// Criando e executando os comandos
-$pedido1 = new PedidoComando("Hambúrguer com batatas fritas", $garcom);
-$pedido2 = new PedidoComando("Salada Caesar", $garcom);
+// Criando os comandos
+$pedidoComando1 = new PedidoComando($pedido1);
+$pedidoComando2 = new PedidoComando($pedido2);
 
-$cozinharPedido1 = new CozinharComando($pedido1->getPedido(), $chef);
-$cozinharPedido2 = new CozinharComando($pedido2->getPedido(), $chef);
+$cozinharComando1 = new CozinharComando($pedido1);
+$cozinharComando2 = new CozinharComando($pedido2);
 
-$servirPedido1 = new ServirComando($pedido1->getPedido(), $garcom);
-$servirPedido2 = new ServirComando($pedido2->getPedido(), $garcom);
+$servirComando1 = new ServirComando($pedido1);
+$servirComando2 = new ServirComando($pedido2);
 
-// Adicionando os pedidos
-$pedido1->executar();  // Pedido 1
-$pedido2->executar();  // Pedido 2
+// Criando o garçom (invocador)
+$garcom = new Garcom();
 
-// Executando os comandos de cozinhar e servir
-$cozinharPedido1->executar();
-$cozinharPedido2->executar();
-$servirPedido1->executar();
-$servirPedido2->executar();
+// Adicionando os comandos na sequência correta
+$garcom->adicionarComando($pedidoComando1);
+$garcom->adicionarComando($pedidoComando2);
+$garcom->adicionarComando($cozinharComando1);
+$garcom->adicionarComando($cozinharComando2);
+$garcom->adicionarComando($servirComando1);
+$garcom->adicionarComando($servirComando2);
 
-$garcom->executarPedidos();  // Mostra o processo final dos pedidos
+// O garçom executa todos os comandos
+$garcom->executarComandos();
